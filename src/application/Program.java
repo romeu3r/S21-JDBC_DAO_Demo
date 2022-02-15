0fe598555885c940406470f0af060ce7c1360c94
romeu3r <romeu3r@gmail.com>
2022-02-15 15:10:34 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,7 @@ public class Program {
     public static void main(String[] args) {
         Department dp = new Department(1, "Books");
         Seller seller = new Seller(1, "Willian", "algo@algo", new Date(), 3000.00, dp);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
